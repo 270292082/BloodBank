8bd27e9ba96b53621607c2074a1e86a9647868f0
Thomas Alexis Wongsokarto <tomwongs.yoobee@gmail.com>
2024-06-04 11:27:45 +1200

    Update intro.cpp
    Trying out a new logo.

--- intro.cpp
+++ intro.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 void IntroDisplay() {
-  cout << "-_____________________________-\t\t" << endl;
-  cout << " ╲⟍                        ⟋ ╱\t\tHelixire Lab" << endl;
-  cout << "  ╲  ⟍                  ⟋   ╱\t\t" << endl;
-  cout << "   ╲    ⟍            ⟋     ╱\t\t" << endl;
-  cout << "    ╲      ⟍      ⟋       ╱\t\t" << endl;
-  cout << "     ╲                   ╱\t\t" << endl;
-  cout << "      ╲        ⤫        ╱\t\t" << endl;
-  cout << "       ╲               ╱\t\t" << endl;
-  cout << "        ╲      |      ╱\t\t\t" << endl;
-  cout << "         ╲     |     ╱\t\t\t" << endl;
-  cout << "          ╲    |    ╱\t\t\t" << endl;
-  cout << "           ╲   |   ╱\t\t\t" << endl;
-  cout << "            ╲  |  ╱\t\t\t" << endl;
-  cout << "             ╲ | ╱\t\t\tLocation: WA Building, 55 Wellesley Street East, Auckland" << endl;
-  cout << "              ╲ ╱\t\t\tContact: 0800 448 325" << endl;
-  cout << "               *\t\t\t" << endl;
-  cout << endl;
+    cout << "-_____________________________-" << endl;
+    cout << " \\  o                     o  /" << endl;
+    cout << "  \\   o                 o   /" << endl;
+    cout << "   \\    o             o    /" << endl;
+    cout << "    \\     o         o     /" << endl;
+    cout << "     \\      o     o      /" << endl;
+    cout << "      \\       o o       /" << endl;
+    cout << "       \\       o       /" << endl;
+    cout << "        \\      o      /" << endl;
+    cout << "         \\     o     /" << endl;
+    cout << "          \\    o    /" << endl;
+    cout << "           \\   o   /" << endl;
+    cout << "            \\  o  /" << endl;
+    cout << "             \\ o /" << endl;
+    cout << "              \\ /" << endl;
+    cout << "               *" << endl;
+    cout << endl;
 };
