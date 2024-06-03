4466fa78b67b45e46ea23256c076193b1921a253
Thomas Alexis Wongsokarto <tomwongs.yoobee@gmail.com>
2024-06-04 11:26:41 +1200

    Update display.cpp
    Trying out new logo for the display function

--- display.cpp
+++ display.cpp
@@ -3,61 +3,61 @@
 using namespace std;
 
 enum pages {
-  logo,
-  intro,
-  donor,
-  donor_reg,
-  login,
-  recipient,
-  recipient_reg,
-  admin,
-  booking
+    logo,
+    intro,
+    donor,
+    donor_reg,
+    login,
+    recipient,
+    recipient_reg,
+    admin,
+    booking
 };
 
 
 // Redirect to the function that will display the correct page.
 void Display(pages page) {
-  
-  switch(page) {
-  case logo:
-    cout << "-_____________________________-" << endl;
-    cout << " ╲⟍                        ⟋ ╱" << endl;
-    cout << "  ╲  ⟍                  ⟋   ╱" << endl;
-    cout << "   ╲    ⟍            ⟋     ╱" << endl;
-    cout << "    ╲      ⟍      ⟋       ╱" << endl;
-    cout << "     ╲                   ╱" << endl;
-    cout << "      ╲        ⤫        ╱" << endl;
-    cout << "       ╲               ╱" << endl;
-    cout << "        ╲      |      ╱" << endl;
-    cout << "         ╲     |     ╱" << endl;
-    cout << "          ╲    |    ╱" << endl;
-    cout << "           ╲   |   ╱" << endl;
-    cout << "            ╲  |  ╱" << endl;
-    cout << "             ╲ | ╱" << endl;
-    cout << "              ╲ ╱" << endl;
-    cout << "               *" << endl;
-    cout << endl;
-    break;
 
-  case intro:
-    IntroDisplay();
-    break;
+    switch (page) {
+    case logo:
+        cout << "-_____________________________-" << endl;
+        cout << " \\  o                     o  /" << endl;
+        cout << "  \\   o                 o   /" << endl;
+        cout << "   \\    o             o    /" << endl;
+        cout << "    \\     o         o     /" << endl;
+        cout << "     \\      o     o      /" << endl;
+        cout << "      \\       o o       /" << endl;
+        cout << "       \\       o       /" << endl;
+        cout << "        \\      o      /" << endl;
+        cout << "         \\     o     /" << endl;
+        cout << "          \\    o    /" << endl;
+        cout << "           \\   o   /" << endl;
+        cout << "            \\  o  /" << endl;
+        cout << "             \\ o /" << endl;
+        cout << "              \\ /" << endl;
+        cout << "               *" << endl;
+        cout << endl;
+        break;
+
+    case intro:
+        IntroDisplay();
+        break;
 
 
-  case donor:
-    break;
-  case donor_reg:
-    break;
-  case login:
-    break;
-  case recipient:
-    break;
-  case recipient_reg:
-    break;
-  case admin:
-    break;
-  case booking:
-    break;
+    case donor:
+        break;
+    case donor_reg:
+        break;
+    case login:
+        break;
+    case recipient:
+        break;
+    case recipient_reg:
+        break;
+    case admin:
+        break;
+    case booking:
+        break;
 
-  }
+    }
 }
