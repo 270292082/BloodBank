deb15da827d987575ecd36f67882c93009a35c88
Thomas Alexis Wongsokarto <tomwongs.yoobee@gmail.com>
2024-06-04 11:43:30 +1200

    Update intro.cpp
    Adding the ASCII Text of  the company

--- intro.cpp
+++ intro.cpp
@@ -2,21 +2,31 @@
 using namespace std;
 
 void IntroDisplay() {
-    cout << "-_____________________________-" << endl;
-    cout << " \\  o                     o  /" << endl;
-    cout << "  \\   o                 o   /" << endl;
-    cout << "   \\    o             o    /" << endl;
-    cout << "    \\     o         o     /" << endl;
-    cout << "     \\      o     o      /" << endl;
-    cout << "      \\       o o       /" << endl;
-    cout << "       \\       o       /" << endl;
-    cout << "        \\      o      /" << endl;
-    cout << "         \\     o     /" << endl;
-    cout << "          \\    o    /" << endl;
-    cout << "           \\   o   /" << endl;
-    cout << "            \\  o  /" << endl;
-    cout << "             \\ o /" << endl;
-    cout << "              \\ /" << endl;
-    cout << "               *" << endl;
+    cout << "-_____________________________-\t\t _    _      _ _      _" << endl;
+    cout << " \\  o                     o  /\t\t| |  | |    | (_)    (_)" << endl;
+    cout << "  \\   o                 o   /\t\t| |__| | ___| |___  ___ _ __ ___" << endl;
+    cout << "   \\    o             o    /\t\t|  __  |/ _ \\ | \\ \\/ / | '__/ _ \\" << endl;
+    cout << "    \\     o         o     /\t\t| |  | |  __/ | |>  <| | | |  __/" << endl;
+    cout << "     \\      o     o      /\t\t|_|  |_|\\___|_|_/_/\\_\\_|_|  \\___|" << endl;
+    cout << "      \\       o o       /\t\t" << endl;
+    cout << "       \\       o       /\t\t" << endl;
+    cout << "        \\      o      /\t\t\t" << endl;
+    cout << "         \\     o     /\t\t\t" << endl;
+    cout << "          \\    o    /\t\t\t" << endl;
+    cout << "           \\   o   /\t\t\t" << endl;
+    cout << "            \\  o  /\t\t\t" << endl;
+    cout << "             \\ o /\t\t\t" << endl;
+    cout << "              \\ /\t\t\t" << endl;
+    cout << "               *\t\t\t" << endl;
     cout << endl;
 };
+
+
+
+
+
+
+
+
+
+
