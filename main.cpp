bf8c22a0cbabd53ac9606f8bef5d1be2a91e254f
270292082 <tomwongs.yoobee@gmail.com>
2024-06-03 16:06:52 +1200

    Rework of the ASCII logo for the display function.

--- main.cpp
+++ main.cpp
@@ -1,39 +1,24 @@
 #include <iostream>
+
 using namespace std;
 
 void display() {
-  cout << "`xnxnxnnnxnxnxnnxnxxnnxnxnxnxnxnnxnxnnxxnxnxnx'" << endl;
-  cout << "  x|-nr                                .xxf{x  " << endl;
-  cout << "   xx   rxn                         uxf   nn   " << endl;
-  cout << "    ~n      nx(                 (xu      n+    " << endl;
-  cout << "     'n^        xn_         ~nx .      In`     " << endl;
-  cout << "      tX(          'xn^ ^xx'.         fn       " << endl;
-  cout << "      f zx             n             nxn       " << endl;
-  cout << "      j  ]n            x           .xn /       " << endl;
-  cout << "      x    x`          n          ?xn; (       " << endl;
-  cout << "      r     n1         n         nn.   ?       " << endl;
-  cout << "      nc    .cx        x        nX ^   ]       " << endl;
-  cout << "      uY      vn       n      ,x+  j   |       " << endl;
-  cout << "              fux'     x     (n        |`      " << endl;
-  cout << "              f  n]    n    nx         v;      " << endl;
-  cout << "                 .)x   x  .n]                  " << endl;
-  cout << "                   |n .x'<n+                   " << endl;
-  cout << "                    nnrnxn)                    " << endl;
-  cout << "                    'xnnxx                     " << endl;
-  cout << "                     !xnx^                     " << endl;
-  cout << "                      ?nn                      " << endl;
-  cout << "                       xn                      " << endl;
-  cout << "                    n  ,j                      " << endl;
-  cout << "                   xxl.'j.                     " << endl;
-  cout << "                       ,r                      " << endl;
-  cout << "                       lu                      " << endl;
-  cout << "                       >c                      " << endl;
-  cout << "                      .~z.                     " << endl;
-  cout << "                       nu                      " << endl;
-  cout << "                       xnf                     " << endl;
-  cout << "                      `nxj                     " << endl;
-
-
+cout << "-_____________________________-" << endl;
+cout << " ╲⟍                        ⟋ ╱" << endl;
+cout << "  ╲  ⟍                  ⟋   ╱" << endl;
+cout << "   ╲    ⟍            ⟋     ╱" << endl;
+cout << "    ╲      ⟍      ⟋       ╱" << endl;
+cout << "     ╲                   ╱" << endl;
+cout << "      ╲        ⤫        ╱" << endl;
+cout << "       ╲               ╱" << endl;
+cout << "        ╲      |      ╱" << endl;
+cout << "         ╲     |     ╱" << endl;
+cout << "          ╲    |    ╱" << endl;
+cout << "           ╲   |   ╱" << endl;
+cout << "            ╲  |  ╱" << endl;
+cout << "             ╲ | ╱" << endl;
+cout << "              ╲ ╱" << endl;
+cout << "               *" << endl;
 }
 
 int main() {
