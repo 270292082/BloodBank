bc48fbd96334c1203e8732deda4e047dcc454a92
Thomas Alexis Wongsokarto <tomwongs.yoobee@gmail.com>
2024-06-04 12:30:12 +1200

    Create user.hpp
    Add a new file for user structure.

---
+++ user.hpp
@@ -0,0 +1,23 @@
+#ifndef USER_HPP
+#define USER_HPP
+
+#include <string>
+using namespace std;
+
+struct users {
+	string username;
+	string password;
+	string dob;
+	string nationality;
+	string ethnicity;
+	string gender;
+	string* conditions;
+	string blood_group;
+	string phone;
+	string email;
+	string location;
+	string last_donation;
+	string category;
+};
+
+#endif
