870970e5118b7539ed39912299c136e6d213f887
Thomas Alexis Wongsokarto <tomwongs.yoobee@gmail.com>
2024-06-04 12:30:49 +1200

    Update intro.cpp
    Updating intro.cpp

--- intro.cpp
+++ intro.cpp
@@ -1,26 +1,67 @@
 #include <iostream>
+#include <string>
+#include "user.hpp"
 using namespace std;
 
-void IntroDisplay() {
-    cout << "-_____________________________-\t\t _    _      _ _      _" << endl;
-    cout << " \\  o                     o  /\t\t| |  | |    | (_)    (_)" << endl;
-    cout << "  \\   o                 o   /\t\t| |__| | ___| |___  ___ _ __ ___" << endl;
-    cout << "   \\    o             o    /\t\t|  __  |/ _ \\ | \\ \\/ / | '__/ _ \\" << endl;
-    cout << "    \\     o         o     /\t\t| |  | |  __/ | |>  <| | | |  __/" << endl;
-    cout << "     \\      o     o      /\t\t|_|  |_|\\___|_|_/_/\\_\\_|_|  \\___|" << endl;
+void IntroDisplay(users user) {
+    if (user.username == "N/A") {
+        cout << endl;
+        cout << "-_____________________________-\t\t _    _      _ _      _             _           _ " << endl;
+        cout << " \\  o                     o  /\t\t| |  | |    | (_)    (_)           | |         | |" << endl;
+        cout << "  \\   o                 o   /\t\t| |__| | ___| |___  ___ _ __ ___   | |     __ _| |__" << endl;
+        cout << "   \\    o             o    /\t\t|  __  |/ _ \\ | \\ \\/ / | '__/ _ \\  | |    / _` | '_ \\" << endl;
+        cout << "    \\     o         o     /\t\t| |  | |  __/ | |>  <| | | |  __/  | |___| (_| | |_) |" << endl;
+        cout << "     \\      o     o      /\t\t|_|  |_|\\___|_|_/_/\\_\\_|_|  \\___|  |______\\__,_|_.__/" << endl;
+        cout << "      \\       o o       /\t\t" << endl;
+        cout << "       \\       o       /\t\t1. Login" << endl;
+        cout << "        \\      o      /\t\t\t2. Register" << endl;
+        cout << "         \\     o     /\t\t\t" << endl;
+        cout << "          \\    o    /\t\t\t" << endl;
+        cout << "           \\   o   /\t\t\t" << endl;
+        cout << "            \\  o  /\t\t\tLocation: WA Building, 55 Wellesley Street East, Auckland" << endl;
+        cout << "             \\ o /\t\t\tP hone: 0800 448 325" << endl;
+        cout << "              \\ /\t\t\t" << endl;
+        cout << "               *\t\t\t" << endl;
+        cout << endl;
+        return;
+    }
+
+    cout << endl;
+    cout << "-_____________________________-\t\t _    _      _ _      _             _           _ " << endl;
+    cout << " \\  o                     o  /\t\t| |  | |    | (_)    (_)           | |         | |" << endl;
+    cout << "  \\   o                 o   /\t\t| |__| | ___| |___  ___ _ __ ___   | |     __ _| |__" << endl;
+    cout << "   \\    o             o    /\t\t|  __  |/ _ \\ | \\ \\/ / | '__/ _ \\  | |    / _` | '_ \\" << endl;
+    cout << "    \\     o         o     /\t\t| |  | |  __/ | |>  <| | | |  __/  | |___| (_| | |_) |" << endl;
+    cout << "     \\      o     o      /\t\t|_|  |_|\\___|_|_/_/\\_\\_|_|  \\___|  |______\\__,_|_.__/" << endl;
     cout << "      \\       o o       /\t\t" << endl;
-    cout << "       \\       o       /\t\t" << endl;
-    cout << "        \\      o      /\t\t\t" << endl;
+    cout << "       \\       o       /\t\tWelcome back " << user.username << "!" << endl;
+    cout << "        \\      o      /\t\t\tYou are a " << user.category << "." << endl;
     cout << "         \\     o     /\t\t\t" << endl;
     cout << "          \\    o    /\t\t\t" << endl;
     cout << "           \\   o   /\t\t\t" << endl;
-    cout << "            \\  o  /\t\t\t" << endl;
-    cout << "             \\ o /\t\t\t" << endl;
+    cout << "            \\  o  /\t\t\tLocation: WA Building, 55 Wellesley Street East, Auckland" << endl;
+    cout << "             \\ o /\t\t\tP hone: 0800 448 325" << endl;
     cout << "              \\ /\t\t\t" << endl;
     cout << "               *\t\t\t" << endl;
     cout << endl;
 };
 
+void Intro() {
+    int choice;
+    IntroDisplay();
+    cout << "Please select an option >> ";
+    cin >> choice;
+
+    switch (choice) {
+    case 1:
+        break;
+    case 2:
+        break;
+    default:
+        cout << "Please, choose a valid option." << endl;
+        break;
+    }
+}
 
 
 
